5a34ee6fbe8e03bf03c7534e9b26e62eed065c71
Zhao Mingxin <1554146770@qq.com>
2023-07-25 00:19:54 +0800

    downloader: upgrade downloader with multiple new features
    1. use progress.log to record download progress and recover downloading when stopped
    2. add priority_list to support user-defined conf/journal downloading preference and order
    3. fix nofile error as previous nofile cannot record failed pdf correctly

--- Downloader.py
+++ Downloader.py
@@ -15,7 +15,9 @@ ROOT_DIR = "/home/zhaomingxin/CodeBase/ConfCollection/RECORDS/"
 LOG_ROOT = "./"
 
 
-PRIORITY_LIST = ["HPCA", "ASPLOS", "CGO", "PLDI", "OSDI"]
+PRIORITY_LIST = ["HPCA", "ASPLOS", "CGO", "PLDI", "OSDI", "USENIX_ATC",
+                 "ISCA", "EuroSys", "PACT", "VEE", "SoCC", "CODES_ISSS",
+                 "ICCD", "HOT_CHIPS"]
 
 
 def prepare_doi_list(list_dir: str):
@@ -48,10 +50,13 @@ def init_log():
     logger.add(log_file, format="[{time}] [{level}]: {message}")
 
 
-def download(root_dir: str):
+def download(root_dir: str, priority_list=None):
     command = "scihub -s "
 
-    conf_jnl_list = os.listdir(root_dir)
+    if priority_list is not None:
+        conf_jnl_list = priority_list
+    else:
+        conf_jnl_list = os.listdir(root_dir)
     for conf_jnl in conf_jnl_list:
         full_path = os.path.join(root_dir, conf_jnl)
         if os.path.isfile(full_path):
@@ -61,9 +66,23 @@ def download(root_dir: str):
             if not os.path.isdir(year_vol_dir):
                 continue
             logger.info("Start downloading: {}-{}".format(conf_jnl, year_vol))
+            progress_file = os.path.join(year_vol_dir, "progress.log")
+            if os.path.exists(progress_file):
+                with open(progress_file, "r") as fp:
+                    cur_progress = fp.read()
+                if cur_progress.isdigit():
+                    skip_idx = int(cur_progress)
+                else:
+                    with open(progress_file, "w") as fp:
+                        fp.write("0")
+                    skip_idx = 0
+            else:
+                skip_idx = 0
+                with open(progress_file, "w") as fp:
+                    fp.write("0")
             if os.path.exists(os.path.join(year_vol_dir, "status.log")):
-                logger.info("{}-{} has processed, skip.".format(conf_jnl,
-                                                                year_vol))
+                logger.info("{}-{} has been processed, skip.".format(conf_jnl,
+                                                                     year_vol))
                 continue
             res = prepare_doi_list(year_vol_dir)
             if res is False:
@@ -75,14 +94,20 @@ def download(root_dir: str):
             if not os.path.exists(pdf_path):
                 os.mkdir(pdf_path)
             failed = list()
-            for doi in res[1]:
+            for doi_idx, doi in enumerate(res[1]):
                 logger.info("Downloading: {}".format(doi[0]))
+                if doi_idx < skip_idx:
+                    logger.info("File {} has been processed, "
+                                "skip it.".format(doi[0]))
+                    continue
                 dres = subprocess.run([command + doi[1] +
                                        " -O {}".format(pdf_path) +
                                        DOWNLOAD_OPTION], shell=True,
                                       stderr=PIPE, stdout=PIPE,
                                       env=os.environ,
                                       check=False, text=True)
+                with open(progress_file, "w") as fp:
+                    fp.write(str(doi_idx + 1))
                 if FAULT_STR in dres.stderr or FAILED_STR in dres.stderr:
                     failed.append(doi[0])
                     logger.warning("Download failed.")
@@ -107,10 +132,10 @@ def download(root_dir: str):
             nofile = os.path.join(pdf_path, "nofile.txt")
             with open(nofile, "w") as fp:
                 for idx, failed_name in enumerate(all_failed):
-                    fp.write(failed_name + "\n" if idx == len(failed_name) - 1
-                             else '')
+                    fp.write(failed_name + ("\n" if idx == len(failed_name) - 1
+                             else ''))
 
 
 if __name__ == "__main__":
     init_log()
-    download(ROOT_DIR)
+    download(ROOT_DIR, PRIORITY_LIST)
